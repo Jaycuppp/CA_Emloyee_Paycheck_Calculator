691d4adebc17a2ed2d21701860785f0a1f18755e
Hakob <hkeshis911@student.glendale.edu>
2023-01-26 00:08:16 -0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-hrs = float(input("Enter Hours:"))
+# Weekly Payrate
+hrs = float(input("Enter Hours Worked In Past 7 Days:"))
 rate = float(input("Enter PayRate:"))
 
 # With Overtime Pay Calculation
@@ -14,26 +15,24 @@ elif hrs <= 40:
     RegularPay = hrs * rate
     ActualPay = RegularPay
     
-print(ActualPay)
+print(f"Here is your paycheck amount for the past week {ActualPay}")
 
 
-def computepay(h, r):
-    h = float(h)
-    r = float(r)
-    if h > 40:
-        Overtime = h - 40
-        RegularTime = h - Overtime
-        OverTimePay = Overtime * r * 1.5
-        RegularPay = RegularTime * r
-        TotalPay = RegularPay + OverTimePay
-        
-    elif h <= 40:
-        TotalPay = h * r
-       
-    return TotalPay
+# Bi-Weekly Payrate
+hrs = float(input("Enter Hours Worked In Past 14 Days:"))
+rate = float(input("Enter PayRate:"))
 
-hrs = input("Enter Hours:")
-r = input("Enter PayRate:")
+# With Overtime Pay Calculation
+if hrs > 80: 
+    Overtime = hrs - 80
+    Regulartime = 80 - Overtime 
+    RegularPay = Regulartime * rate
+    OvertimePay = Overtime * rate * 1.5
+    ActualPay = RegularPay + OvertimePay
 
-p = computepay(hrs, r)
-print("Pay", p)
+# No Overtime Pay Calculation
+elif hrs <= 80:
+    RegularPay = hrs * rate
+    ActualPay = RegularPay
+    
+print(f"Here is your paycheck amount for the past week {ActualPay}")
